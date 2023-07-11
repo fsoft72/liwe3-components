28190037fccbd4f9159ace25e4b47ebd09be5e1c
fsoft <fabio.rotondo@gmail.com>
2023-07-11 10:11:52 +0200

    Add: Grow Button

---
+++ src/components/GrowButton/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export interface GrowButtonProps {
+	label: string;
+
+	onClick?: () => void;
+}
+
+const GrowButton = ( { label, onClick }: GrowButtonProps ) => {
+	return (
+		<button onClick={onClick} className="liwe3-grow-button box">
+			<span className="circle" aria-hidden="true">
+				<span className="icon arrow"></span>
+			</span>
+			<span className="text">{label}</span>
+		</button >
+	);
+};
+
+export default GrowButton;
