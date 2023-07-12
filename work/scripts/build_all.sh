ed46f3dc247c8b6f99abd3626bd6352d4d5f245f
Fabio Rotondo <fabio.rotondo@gmail.com>
2023-07-12 09:30:11 +0200

    Build all now uses dest/components dir

--- work/scripts/build_all.sh
+++ work/scripts/build_all.sh
@@ -3,20 +3,18 @@
 # This script builds all components from source code.
 # It gets the components directory as an argument and builds all components into DESTINATION_DIR.
 
-# if no argument is provided, print usage and exit
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <destination directory>"
-    echo ""
-    exit 1
-fi
-
 # Check if we are in the project root directory
 if [ ! -e single-component-builder ]; then
     echo "Please run this script from the project root directory."
     exit 1
 fi
 
-DESTINATION_DIR=$1
+mkdir -p dist/components
+
+# Get current working directory
+CURRENT_DIR=$(pwd)
+
+DESTINATION_DIR=$CURRENT_DIR/dist/components
 
 # Cycle through all components in the components directory
 for COMPONENT_NAME in src/components/*
