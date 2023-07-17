77b0995b731af31f4c8b3dd51823047477874c57
pigraphic <developing@pig-fx.it>
2023-07-17 13:43:49 +0200

    ThemeSwitcher

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -11,29 +11,9 @@ import "../src/components/DataGrid/styles.css";
 import "../src/components/Paginator/styles.css";
 import "../src/components/Modal/styles.css";
 import "../src/components/ThemeSwitcher/styles.css";
-import ThemeContext from "../src/components/ThemeSwitcher/ThemeContext";
 import ThemeSwitcher from "../src/components/ThemeSwitcher";
 
 /** @type { import('@storybook/react').Preview } */
-type WrapperProps = {
-  children: ReactNode;
-}
-const Wrapper = ({ children }: WrapperProps) => {
-  const {theme} = React.useContext(ThemeContext);
-  const [themeState, setThemeState] = React.useState(theme);
-  return (
-  <>
-    <div className={`storybook-wrapper ${themeState}`}>
-      <div style={{minHeight:50}}>
-        <ThemeSwitcher setTheme={setThemeState}/>
-      </div>
-      <ThemeContext.Provider value={{theme:themeState}}>
-          {children}
-      </ThemeContext.Provider>
-    </div>
-  </>
-  );
-}
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -46,9 +26,14 @@ const preview = {
   },
   decorators: [
     ( Story ) => (
-      <Wrapper>
-        <Story />
-      </Wrapper>
+      <>
+        <div className={'storybook-top-bar'}>
+          <ThemeSwitcher /><div>&nbsp;ThemeSwitcher</div>
+        </div>
+        <div>
+          <Story />
+        </div>
+      </>
     ),
   ],
 };
--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,23 +1,27 @@
-import React, { useContext, useState } from 'react';
-import { ThemeContext } from './ThemeContext';
-
+import React from 'react';
+interface ThemeSwitcherProps {
+	lightTheme?: string,
+	darkTheme?: string,
+}
 // Create a component that allows the user to switch between the light and dark theme
-export  default function ThemeSwitcher (props) {
-	const setTheme = props.setTheme;
-	console.log('---  ThemeSwitcher', typeof setTheme);
-
+export  default function ThemeSwitcher ( {lightTheme = 'liwe-light-theme', darkTheme = 'liwe-dark-theme'}: ThemeSwitcherProps) {
 	const toggleTheme = ( e: any ) => {
+		const body = document.querySelector( 'body' );
+		const classes = body?.getAttribute('class');
+		let newClasses = '';
 		if ( e.target.checked ) {
-			setTheme( 'liwe-dark-theme' );
+			newClasses = classes?.replace( lightTheme, darkTheme) || darkTheme;
+			body?.setAttribute('class', newClasses );
 		} else {
-			setTheme( 'liwe-light-theme' );
+			newClasses = classes?.replace( darkTheme, lightTheme ) || 'liwe-light-theme';
+			body?.setAttribute('class', newClasses );
 		}
 	};
 
 	return (
-		<label className="switch">
-			<input type="checkbox" onChange={toggleTheme} />
-			<span className="slider"></span>
+		<label className="liwe3-themeswitch">
+			<input type="checkbox" onChange={toggleTheme}/>
+			<span className="liwe3-themeswitch-slider"></span>
 		</label>
 	);
 }
