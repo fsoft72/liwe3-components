7f83d1257078afdbf92d8b9b906e65279ef1ad81
fsoft <fabio.rotondo@gmail.com>
2023-07-18 11:38:46 +0200

    Removed the setDark parameter.  The ThemeSwitcher determines its initial state from body classes

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,25 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 interface ThemeSwitcherProps {
 	lightTheme?: string,
 	darkTheme?: string,
-	setDark?: boolean	
 }
 // Create a component that allows the user to switch between the light and dark theme
-export  default function ThemeSwitcher ( {lightTheme = 'liwe-light-theme', darkTheme = 'liwe-dark-theme',  setDark= false}: ThemeSwitcherProps) {
-	const [isDark, setIsDark] = useState( setDark );
+export default function ThemeSwitcher ( { lightTheme = 'liwe-light-theme', darkTheme = 'liwe-dark-theme' }: ThemeSwitcherProps ) {
+	const [ isDark, setIsDark ] = useState( false );
+
+	useEffect( () => {
+		// check in the body class if the dark theme is set
+		const body = document.querySelector( 'body' );
+		const classes = body?.getAttribute( 'class' );
+
+		if ( classes?.includes( darkTheme ) ) {
+			setIsDark( true );
+		} else {
+			setIsDark( false );
+		}
+	}, [] );
+
 	const toggleTheme = ( e: any ) => {
 		console.log( e.target.checked );
 		const body = document.querySelector( 'body' );
-		const classes = body?.getAttribute('class');
+		const classes = body?.getAttribute( 'class' );
 		let newClasses = '';
 		if ( e.target.checked ) {
 			setIsDark( true );
-			newClasses = classes?.replace( lightTheme, darkTheme) || darkTheme;
-			body?.setAttribute('class', newClasses );
+			newClasses = classes?.replace( lightTheme, darkTheme ) || darkTheme;
+			body?.setAttribute( 'class', newClasses );
 		} else {
 			setIsDark( false );
-			newClasses = classes?.replace( darkTheme, lightTheme ) || 'liwe-light-theme';
-			body?.setAttribute('class', newClasses );
+			newClasses = classes?.replace( darkTheme, lightTheme ) || lightTheme;
+			body?.setAttribute( 'class', newClasses );
 		}
 	};
 
