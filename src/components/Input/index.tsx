6e1140126f6ab47cc529b9fe1e1c27cf1bafa3af
pigraphic <developing@pig-fx.it>
2023-08-01 01:51:48 +0200

    ADD: filter value also on change for autocomplete

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -34,14 +34,24 @@ const sizeMap = {
  */
 const Input = forwardRef(( props: InputProps, ref: React.Ref<HTMLInputElement>) => {
 	const { label, name, filter, size, mode, status, message, attrs, ...rest } = props;	
-    const regex = filter ? new RegExp(filter) : null;
+    const regExpChar = filter ? new RegExp(filter) : null;
+    const regExpValue = filter ? new RegExp(filter, 'g') : null;
+
 
     const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement> & { data:string}) => {
-        if (!regex) return;
+        if (!regExpChar) return;
         const char = e?.data;
-        if (!regex.test(char)) {
+        if (!regExpChar.test(char)) {
             e.preventDefault();
         }
+    };
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (regExpValue) {
+            const filtered = e?.target?.value.match(regExpValue)?.join('') || '';
+            e.target.value = filtered;
+        }
+        if (props.onChange) props.onChange(e);
     };
 
 	const prepCssClasses = () => {
@@ -61,6 +71,7 @@ const Input = forwardRef(( props: InputProps, ref: React.Ref<HTMLInputElement>) 
                 {...attrs} 
                 {...rest}
                 onBeforeInput={handleKeyPress}
+                onChange={handleOnChange}
             />
             <div className={cssClasses.status}>{message}</div>
         </div>
